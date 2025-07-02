508ef825f54b7ed908bf552ad9fedf60d6842888
vickydepicard <vickyhotio98@gmail.com>
2025-07-02 11:37:01 +0100

    Fix :Corrige chemin module pour GitHub Actions

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 module "s3" {
-  source         = "/home/suphotio/Terraform/lab-terraform-aws/modules/s3_bucket"
+  source = "../../modules/s3_bucket"
   environment    = var.environment
   bucket_suffix  = "lab-terraform-aws"
 }
