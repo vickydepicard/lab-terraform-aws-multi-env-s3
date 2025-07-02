15c3f1c36660fdb9a0fe7addf702729569eaaa30
vickydepicard <vickyhotio98@gmail.com>
2025-07-02 11:41:21 +0100

    Fix: terraform fmt

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -3,8 +3,7 @@ provider "aws" {
 }
 
 module "s3" {
-  source = "../../modules/s3_bucket"
-  environment    = var.environment
-  bucket_suffix  = "lab-terraform-aws"
+  source        = "../../modules/s3_bucket"
+  environment   = var.environment
+  bucket_suffix = "lab-terraform-aws"
 }
-
